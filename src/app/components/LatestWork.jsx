aa403686d06a1cf34a106317537f8dd56a674d22
shantunu007 <chakmashantunu.web@gmail.com>
2025-10-05 20:47:41 +0600

    our latest work section done

---
+++ src/app/components/LatestWork.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function LatestWork() {
+    return (
+        <div>
+            <div className='max-w-11/12 mx-auto mt-24'>
+                <div>
+                    <div className='flex justify-between items-center'>
+                        <h1 className='font-bold text-5xl'>Our Latest Work</h1>
+                        <p className='w-2xl'>Based on the description of Metro Solver and the image provided, here is a 6-step process that Metro Solver uses to scale a customer's business</p>
+                    </div>
+                </div>
+
+            </div>
+            <img src="/latestImage.png" alt="" className='w-full my-12' />
+        </div>
+    )
+}
